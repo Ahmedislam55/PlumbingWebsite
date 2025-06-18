ab8de52a0cc54bd227fed87f8d55e93d9163f434
Ahmed islam <105564983+Ahmedislam55@users.noreply.github.com>
2025-06-18 14:08:02 +0300

    page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,7 +4,11 @@ import AboutNumber from "./AboutNumber/AboutNumber";
 import AboutUs from "./AboutUs/AboutUs";
 import OurAwards from "./OurAwards/OurAwards";
 import SolvePlumbing from "./SolvePlumbing/SolvePlumbing";
-
+export const metadata = {
+  title: "Plumbing Repair About",
+  description:
+    "Reliable and affordable plumbing repair solutions for homes and businesses. Contact us now for expert service.",
+};
 export default function page() {
   return (
     <>
