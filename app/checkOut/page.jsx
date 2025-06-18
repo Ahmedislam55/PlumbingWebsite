39f9ee0dc9e1a8d28b6ac88c55e4d2a0523412d3
Ahmed islam <105564983+Ahmedislam55@users.noreply.github.com>
2025-06-18 14:11:46 +0300

    page.jsx

--- app/checkOut/page.jsx
+++ app/checkOut/page.jsx
@@ -1,6 +1,10 @@
 import CheckOutHome from "./CheckOutHome/CheckOutHome";
 import DetailsPage from "./DetailsPage/DetailsPage";
-
+export const metadata = {
+  title: "Plumbing Repair Check Out",
+  description:
+    "Reliable and affordable plumbing repair solutions for homes and businesses. Contact us now for expert service.",
+};
 export default function page() {
   return (
     <div>
