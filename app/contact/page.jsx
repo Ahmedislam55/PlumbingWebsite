edfbab53661ace585979d2dfa0d850ef46adc658
Ahmed islam <105564983+Ahmedislam55@users.noreply.github.com>
2025-06-18 14:12:14 +0300

    page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,11 @@
 import HaveQuestions from "../home/HaveQuestions/HaveQuestions";
 import ContactHome from "./ContactHome/ContactHome";
 import GoogleMapComponent from "./LocationPage/LocationPage";
-
+export const metadata = {
+  title: "Plumbing Repair Contact",
+  description:
+    "Reliable and affordable plumbing repair solutions for homes and businesses. Contact us now for expert service.",
+};
 export default function page() {
   return (
     <>
