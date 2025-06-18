593bfb6d00a363412193506964c0baa978401d2e
Ahmed islam <105564983+Ahmedislam55@users.noreply.github.com>
2025-06-18 14:12:47 +0300

    page.jsx

--- app/getQoute/page.jsx
+++ app/getQoute/page.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import GetQouteHome from './GetQouteHome/GetQouteHome'
 import QoutePlumbing from './QoutePlumbing/QoutePlumbing'
 import IconsHome from '../home/Icons/IconsHome'
-
+export const metadata = {
+  title: "Plumbing Repair Get Qoute",
+  description:
+    "Reliable and affordable plumbing repair solutions for homes and businesses. Contact us now for expert service.",
+};
 export default function page() {
   return (
     <div className='bg-gray-100'>
