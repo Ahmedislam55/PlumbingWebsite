483a5985b120b029dfaf61527c6dd918f1d7f153
Ahmed islam <105564983+Ahmedislam55@users.noreply.github.com>
2025-06-18 14:17:19 +0300

    page.jsx

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,6 +1,10 @@
 import ProductPage from "./ProductPage/ProductPage";
 import ShopHome from "./ShopHome/CategoriesHome";
-
+export const metadata = {
+  title: "Plumbing Repair Shop",
+  description:
+    "Reliable and affordable plumbing repair solutions for homes and businesses. Contact us now for expert service.",
+};
 export default function page() {
   return (
     <div className="bg-gray-100">
