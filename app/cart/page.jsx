fc31ffc16059fb5935a9414ba8e427fa49be97e4
Ahmed islam <105564983+Ahmedislam55@users.noreply.github.com>
2025-06-18 14:10:48 +0300

    page.jsx

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,6 +1,10 @@
 import CartHome from "./CartHome/CartHome";
 import DisplayCart from "./displayCart/DisplayCart";
-
+export const metadata = {
+  title: "Plumbing Repair Cart",
+  description:
+    "Reliable and affordable plumbing repair solutions for homes and businesses. Contact us now for expert service.",
+};
 export default function page() {
   return (
     <div>
